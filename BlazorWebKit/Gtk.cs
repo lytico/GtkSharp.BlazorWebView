e2e0dab1dfd957cf1cac55b3a79ac2112db4889d
lytico <lytico@users.sourceforge.net>
2023-08-25 17:23:12 +0200

    Gtk: replace GConnectFlags with GLib.ConnectFlags & remove unused GtkWindowType

--- BlazorWebKit/Gtk.cs
+++ BlazorWebKit/Gtk.cs
@@ -6,24 +6,12 @@ internal static class Gtk
 {
     public const string FilePath = "gtk";
 
-    public enum GtkWindowType : int
-    {
-        GTK_WINDOW_TOPLEVEL,
-        GTK_WINDOW_WINDOW_POPUP
-    }
-
-    public enum GConnectFlags : int
-    {
-        G_CONNECT_AFTER,  	 
-        G_CONNECT_SWAPPED
-    }
-
     [DllImport(FilePath)]
-    public static extern ulong g_signal_connect_data (IntPtr instance, string detailed_signal, IntPtr c_handler, IntPtr data, IntPtr destroy_data, GConnectFlags connect_flags);
+    public static extern ulong g_signal_connect_data (IntPtr instance, string detailed_signal, IntPtr c_handler, IntPtr data, IntPtr destroy_data, GLib.ConnectFlags connect_flags);
 
     public static ulong g_signal_connect(IntPtr instance, string detailed_signal, IntPtr c_handler, IntPtr data)
     {
-        return g_signal_connect_data(instance, detailed_signal, c_handler, data, IntPtr.Zero,  (GConnectFlags)0);
+        return g_signal_connect_data(instance, detailed_signal, c_handler, data, IntPtr.Zero,  (GLib.ConnectFlags)0);
     }
 
     [DllImport(FilePath)]
