c47ce3be8744356f0128c37ff67072ea6f5524b3
lytico <lytico@users.sourceforge.net>
2023-08-31 02:42:22 +0200

    use with upstream https://github.com/GtkSharp/GtkSharp/pull/418 & https://github.com/GtkSharp/GtkSharp/pull/421

--- BlazorWebKit/GtkSharpWebViewManager.cs
+++ BlazorWebKit/GtkSharpWebViewManager.cs
@@ -1,5 +1,5 @@
 using System.Web;
-using GtkSharpUpstream;
+using GLib;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
@@ -8,10 +8,7 @@ using WebKit;
 
 namespace BlazorWebKit;
 
-public class GtkSharpWebViewManager : Microsoft.AspNetCore.Components.WebView.WebViewManager
-{
-
-    delegate void WebMessageHandler(IntPtr contentManager, IntPtr jsResult, IntPtr arg);
+public class GtkSharpWebViewManager : Microsoft.AspNetCore.Components.WebView.WebViewManager {
 
     const string _scheme = "app";
     readonly static Uri _baseUri = new Uri($"{_scheme}://localhost/");
@@ -20,23 +17,20 @@ public class GtkSharpWebViewManager : Microsoft.AspNetCore.Components.WebView.We
         : base(serviceProvider, Dispatcher.CreateDefault(), _baseUri
             , new PhysicalFileProvider(serviceProvider.GetRequiredService<BlazorWebViewOptions>().ContentRoot)
             , new()
-            , serviceProvider.GetRequiredService<BlazorWebViewOptions>().RelativeHostPath)
-    {
+            , serviceProvider.GetRequiredService<BlazorWebViewOptions>().RelativeHostPath) {
         var options = serviceProvider.GetRequiredService<BlazorWebViewOptions>();
         _relativeHostPath = options.RelativeHostPath;
         _rootComponent = options.RootComponent;
         _logger = serviceProvider.GetService<ILogger<BlazorWebView>>();
 
         WebView = webView;
-        HandleWebMessageDelegate = HandleWebMessage;
 
         // This is necessary to automatically serve the files in the `_framework` virtual folder.
         // Using `file://` will cause the webview to look for the `_framework` files on the file system,
         // and it won't find them.
         WebView.Context.RegisterUriScheme(_scheme, HandleUriScheme);
 
-        Dispatcher.InvokeAsync(async () =>
-        {
+        Dispatcher.InvokeAsync(async () => {
             await AddRootComponentAsync(_rootComponent, "#app", ParameterView.Empty);
         });
 
@@ -56,95 +50,82 @@ public class GtkSharpWebViewManager : Microsoft.AspNetCore.Components.WebView.We
                     window.__receiveMessageCallbacks.push(callback);
                 }
             };
-            """, 
+            """,
             UserContentInjectedFrames.AllFrames,
             UserScriptInjectionTime.Start,
             null, null);
 
         WebView.UserContentManager.AddScript(script);
- 
-        WebView.UserContentManager.SignalConnectData("script-message-received::webview",
-            HandleWebMessageDelegate, 
-            IntPtr.Zero,IntPtr.Zero, (global::GLib.ConnectFlags)0);
 
         WebView.UserContentManager.RegisterScriptMessageHandler("webview");
+
+        WebView.UserContentManager.ScriptMessageReceived += (o, args) => {
+            var jsValue = args.JsResult.JsValue;
+
+            if (jsValue.IsString) {
+
+                var s = jsValue.ToString();
+
+                if (s is not null) {
+                    _logger?.LogDebug($"Received message `{s}`");
+
+                    try {
+                        MessageReceived(_baseUri, s);
+                    } finally { }
+                }
+            }
+        };
 
         Navigate("/");
     }
 
     public WebView WebView { get; init; }
-    readonly WebMessageHandler HandleWebMessageDelegate;
+
     readonly string _relativeHostPath;
     readonly Type _rootComponent;
     readonly ILogger<BlazorWebView>? _logger;
 
-    void HandleUriScheme(URISchemeRequest request)
-    {
-        if (request.Scheme != _scheme)
-        {
+    void HandleUriScheme(URISchemeRequest request) {
+        if (request.Scheme != _scheme) {
             throw new Exception($"Invalid scheme \"{request.Scheme}\"");
         }
 
         var uri = request.Uri;
-        if (request.Path == "/")
-        {
+
+        if (request.Path == "/") {
             uri += _relativeHostPath;
         }
 
         _logger?.LogInformation($"Fetching \"{uri}\"");
 
-        if (TryGetResponseContent(uri, false, out int statusCode, out string statusMessage, out Stream content, out IDictionary<string, string> headers)) 
-        {
+        if (TryGetResponseContent(uri, false, out int statusCode, out string statusMessage, out Stream content, out IDictionary<string, string> headers)) {
 
-            using var inputStream = content.AsInputStream();
-            request.Finish(inputStream, content.Length, headers["Content-Type"]);
+            using var memoryStream = new MemoryStream();
+            var length = (int)content.Length;
+            content.CopyTo(memoryStream, length);
+            var buffer = memoryStream.GetBuffer();
+            Array.Resize(ref buffer, length);
+            using var inputStream = InputStream.NewFromData(buffer);
 
+            request.Finish(inputStream, length, headers["Content-Type"]);
 
-        } 
-        else
-        {
+        } else {
             throw new Exception($"Failed to serve \"{uri}\". {statusCode} - {statusMessage}");
         }
     }
 
-    void HandleWebMessage(IntPtr contentManager, IntPtr jsResultHandle, IntPtr arg) {
-            
-        var jsResult = new JavascriptResult(jsResultHandle);
-        
-        var jsValue = jsResult.JsValue;
-        if (jsValue.IsString) 
-        {
-                
-            var s = jsValue.ToString();
-            if (s is not null)
-            {
-                _logger?.LogDebug($"Received message `{s}`");
-
-                try
-                {
-                    MessageReceived(_baseUri, s);
-                }
-                finally
-                {
-                }
-            }
-        }
-
-    }
-
-    protected override void NavigateCore(Uri absoluteUri)
-    {
+    protected override void NavigateCore(Uri absoluteUri) {
         _logger?.LogInformation($"Navigating to \"{absoluteUri}\"");
 
         WebView.LoadUri(absoluteUri.ToString());
     }
 
-    protected override void SendMessage(string message)
-    {
+    protected override void SendMessage(string message) {
         _logger?.LogDebug($"Dispatching `{message}`");
 
         var script = $"__dispatchMessageCallback(\"{HttpUtility.JavaScriptStringEncode(message)}\")";
 
         WebView.RunJavascript(script);
     }
+
 }
